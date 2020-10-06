2eb3975a88934b4a82dacb9873f20252d4856fa5
yechubot <61838900+yechubot@users.noreply.github.com>
2020-10-06 17:18:06 +0900

    Add files via upload

--- js/chat.js
+++ js/chat.js
@@ -3,7 +3,7 @@ $('li').hide();
 $('li').each(function(index){
     $(this).delay(index*1000).fadeIn();
 })
-
+//click msg to change color
 $('.left').click(function(){
     $(this).toggleClass('gray');
 }).css('cursor','pointer');
@@ -12,11 +12,39 @@ $('.right').click(function(){
     $(this).toggleClass('orange');
 }).css('cursor','pointer');
 
-// $('#hideBtn').click(function(){
-//     $('#messages').slideToggle(500);
-// })
+//click to show and hide msg 
+//--> if i use html with same class whole msg is replaced, so maybe I should add sth
+$('#hideBtn').click(function(){
+    $('#messages').toggleClass('hide');
 
-let clicked = true;
+    if($('#messages').hasClass('hide')){
+        $(this).html('Click to show messages');
+        $('#chatBox').append("<h2>nothing to show >_<</h2>").addClass('nothing');
+    }else {
+        $(this).html('Click to hide messages');
+        $('h2:contains("nothing")').remove(); 
+        $('#chatBox').removeClass();
+    }
+})
+
+
+// using button value...? not sure 
+/*$('#hideBtn').click(function(){
+    
+    let label = $('#hideBtn').value();
+    
+    if(label =="hide"){
+        $('#messages').value("show");
+        $('#messages').hide();
+    } else {
+        $('#messages').value("hide");
+        $('#messages').show();
+    }
+    });
+*/
+
+// disapper whole data when you click?--> should add all the msg again with html... hmm too messy 
+/*let clicked = true;
 $('#hideBtn').click(function(){
     if(clicked){
         $('#messages').addClass('hide');
@@ -24,8 +52,9 @@ $('#hideBtn').click(function(){
         $(this).html('Click to show messages')
         clicked = false;
     }else {
-        $('#messages').removeClass();
+        $('#messages').removeClass('hide','show');
         $(this).html('Click to hide messages')
         clicked = true;
     }
-})
+});
+*/
